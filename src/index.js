657787a1bb8f892bd78ffa632a7db1079df9cee2
DipokSarker <dipok.ronjan.sarker@gmail.com>
2023-03-22 23:39:03 +0200

    props added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,6 +5,4 @@ import App from './App';
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-
-
-root.render(<App/>);
+root.render(<App />);
